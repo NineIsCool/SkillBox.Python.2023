fb6d1fe41c2e4ca73cb8fcf19b2f396de479854c
NineIsCool <117851483+NineIsCool@users.noreply.github.com>
2023-11-07 14:22:12 +0500

    hw-07

---
+++ buns/mod7/task1.py
@@ -0,0 +1,26 @@
+def validate_args(func):
+    def wrapper(*args, **kwargs):
+        if len(args) != 1:
+            return "Not enough arguments" if len(args) < 1 else "Too many arguments"
+
+        arg = args[0]
+        if not isinstance(arg, int):
+            return "Wrong types"
+        if arg < 0:
+            return "Negative argument"
+
+        return func(*args, **kwargs)
+
+    return wrapper
+
+
+@validate_args
+def my_function(n):
+    return n * 2
+
+
+print(my_function())
+print(my_function(10, 20))
+print(my_function(3.2))
+print(my_function(-5))
+print(my_function(5))
